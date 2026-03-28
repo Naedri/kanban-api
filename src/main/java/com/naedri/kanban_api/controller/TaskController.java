0a4a09700a069d673b32daef125f4ef1e390e156
JALLAIS Adrien <adrien.jallais@protonmail.com>
2026-03-28 11:49:10 +0100

    feat(controller): TaskController

---
+++ src/main/java/com/naedri/kanban_api/controller/TaskController.java
@@ -0,0 +1,38 @@
+package com.naedri.kanban_api.controller;
+
+import com.naedri.kanban_api.domain.CreateTaskRequest;
+import com.naedri.kanban_api.domain.dto.CreateTaskRequestDto;
+import com.naedri.kanban_api.domain.dto.TaskDto;
+import com.naedri.kanban_api.domain.entity.Task;
+import com.naedri.kanban_api.mapper.TaskMapper;
+import com.naedri.kanban_api.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/rest/v1/task")
+public class TaskController {
+    private final TaskService taskService;
+    private final TaskMapper taskMapper;
+
+    public TaskController(TaskService taskService, TaskMapper taskMapper) {
+        this.taskService = taskService;
+        this.taskMapper = taskMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskDto> createTask(
+            @Valid
+            @RequestBody
+            CreateTaskRequestDto createTaskRequestDto) {
+        CreateTaskRequest createTaskRequest = taskMapper.fromDto(createTaskRequestDto);
+        Task task = taskService.createTask(createTaskRequest);
+        TaskDto createdTaskDto = taskMapper.toDto(task);
+        return new ResponseEntity<>(createdTaskDto, HttpStatus.CREATED);
+    }
+}
